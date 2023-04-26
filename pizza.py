6c99402f18e19a2bd2018b53b53c51b2534fb314
cycycy1001 <124072380+cycycy1001@users.noreply.github.com>
2023-04-26 19:20:37 -0400

    Update pizza.py

--- pizza.py
+++ pizza.py
@@ -1,23 +1,6 @@
-import uuid
+import random
 
 print ("The Pizza Place")
-
-def game ():
-    game = input ('Do you want to play as a cook or cashier: ')
-    if game == 'cook':
-        print ('sajoifj')
-    elif game == 'cashier':
-        print ('sdiofjod')
-
-def start ():
-    start = input ('Do you want to play? Y/N: ')
-    if start == 'Y':
-            print ('Its time to cook')
-    elif start == 'N' or 'n':
-         print ('Maybe next time :(')
-
-
-start ()
 
 class worker:
     def __init__(self, name, age, wage):
@@ -25,7 +8,6 @@ class worker:
         self.age = age
         self.wage = wage
 
-        
 class cashier (worker):
     def __init__(self, name, age, wage, job):
         super().__init__(name, age, wage)
@@ -34,4 +16,31 @@ class cook (worker):
     def __init__(self, name, age, wage, job):
         super().__init__(name, age, wage)
         self.job = job
+
+Jonathan = cashier ('Jonathan Chan', 15, 15, 'To take orders from customers.')
+Yi = cook ('Yi Cheng', 15, 20, 'To cook pizza for the customers.')
+
+def cookgame ():
+    toppings = ["cheese", "pepperoni","sausage"]
+    order = random.choice(toppings)
+    print(f"Your customer has ordered a {order} pizza!")
+
+def main ():
+    game = input ('Who would you like to play as? Jonathan Chan (cashier) or Yi Cheng (cook)')
+    if game.capitalize == 'cook' or 'Yi Cheng':
+        print ("You've chosen the cook!")
+        cookgame ()
+    else:
+        print ("You've chosen the cashier!")
+
+
+def start ():
+    start = input ('Do you want to play? y/n')
+    if start == 'y':
+        print ('Get ready to work!') 
+        main ()
+    else:
+        print ('Too bad, maybe next time')
+
+start ()
 
