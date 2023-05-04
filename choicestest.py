cf258da96c9a016dfb62e557d3ead043da9b74e5
cycycy1001 <124072380+cycycy1001@users.noreply.github.com>
2023-05-04 10:31:54 -0400

    Update choicestest.py

--- choicestest.py
+++ choicestest.py
@@ -5,21 +5,37 @@ def questions ():
     question1 = {
         "question": 'Customer: "Hello!"',
         "choices": ['1. "Hello! What would you like to order?"','2. "Leave the store now!"', '3. "Hey! What would you like to try today?"'],
-        "answer": "2"
+        "answer": "1"  
+        
     }
+    question2 = {
+        "question": 'Customer: "Hello!"',
+        "choices": ['1. "Hey there! What pizza would you like to order?"','2. "Welcome to The Pizza Place! How may I help you?"', '3. "Skedaddle outta here! We do not like you!"'],
+        "answer": "3"
+    }
+    question3 = {
+        "question": 'Customer: "Hello!"',
+        "choices": ['1. "Hello valued customer! How would you like to LEAVE!"','2. "Hello, my name is Jonathan. What would you like to order?"', '3. "Welcome to The Pizza Place! Take a order prease?"'],
+        "answer": "1"
+    }
+
+
 
     # Print the question and answer choices
     print(question1["question"])
     for choice in question1["choices"]:
         print(choice)
 
+    
     # Ask the user to select an answer
     user_answer = input("What will your response be? ")
 
     # Check if the answer is correct
     if user_answer == question1["answer"]:
-        print('Customer: "Rude! I will be taking my business elsewhere!"')
-    else:
         print('Customer: "I would like a pepperoni pizza!"')
+    else:
+        print('Customer: "How rude! I will be taking my business elsewhere!"')
+        
+        
 
 questions ()
