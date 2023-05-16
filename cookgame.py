39609e9a96e41286e6cbf19fdc7102c3c8dd5d49
cycycy1001 <124072380+cycycy1001@users.noreply.github.com>
2023-05-16 10:58:27 -0400

    s

--- cookgame.py
+++ cookgame.py
@@ -1,20 +1,7 @@
 import random
 import sys,time
 
-class worker:
-    def __init__(self, name, age, wage):
-        self.name = name
-        self.age = age
-        self.wage = wage
 
-class cashier (worker):
-    def __init__(self, name, age, wage, job):
-        super().__init__(name, age, wage)
-        self.job = job
-class cook (worker):
-    def __init__(self, name, age, wage, job):
-        super().__init__(name, age, wage)
-        self.job = job
 def sprint(str):
    for c in str + '\n':
      sys.stdout.write(c)
