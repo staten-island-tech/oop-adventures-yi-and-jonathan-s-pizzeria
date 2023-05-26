0454635d44c4768aa877a23c61dd390db8cb726f
cycycy1001 <124072380+cycycy1001@users.noreply.github.com>
2023-05-26 10:54:08 -0400

    fs

--- cashiergame copy.py
+++ cashiergame copy.py
@@ -44,58 +44,66 @@ def tips():
 def questions ():
     options = [
         {
-            "dialogue": 'Customer: "Hello!"',
+            "dialogue": 'Customer: "Hello!"\n',
             "choices": [
-                '1. "Hello! What would you like to order?"',
-                '2. "What do you want?"',
-                '3. "We don\'t like your kind around here! Skedaddle!"'
+                '1. "Hello! What would you like to order?"\n',
+                '2. "What do you want?"\n',
+                '3. "We don\'t like your kind around here! Skedaddle!"\n'
             ],
             "correct_choice": 1
         },
         {
-            "dialogue": 'Customer: "So many options..."',
+            "dialogue": 'Customer: "So many options..."\n',
             "choices": [
-                '1. "Leave the store please, your face is unpleasing"',
-                '2. "Stop bothering me."',
-                '3. "Hey there, what pizza would you like to order?"'
+                '1. "Leave the store please, your face is unpleasing."\n',
+                '2. "Stop bothering me."\n',
+                '3. "Hey there, what pizza would you like to order?"\n'
             ],
             "correct_choice": 3
         },
         {
-            "dialogue": 'Customer: "Hey! Lemme just think about what I want."',
+            "dialogue": 'Customer: "Hey! Lemme just think about what I want."\n',
             "choices": [
-                '1. "I don\'t get paid enough to deal with people like you"',
-                '2. "Take your time, valued customer!"',
-                '3. "I don\'t have time... Chop chop buddy!."'
+                '1. "I don\'t get paid enough to deal with people like you"\n',
+                '2. "Take your time, valued customer!"\n',
+                '3. "I don\'t have time... Chop chop buddy!."\n'
             ],
             "correct_choice": 2
         }
     ]
-
+    customerresponses =[
+        'Customer: "How rude! I will be taking my business elsewhere!"',
+        'Customer: "Where is your manager?"',
+        'Customer: "All I wanted was a pizza..."'
+    ]
     selected_option = random.choice(options)
     print(selected_option["dialogue"])
     for choice in selected_option["choices"]:
         print(choice)
+    
 
     user_choice = input("Enter a response: ")
+    print ('\n')
 
     if user_choice == str(selected_option["correct_choice"]):
-        print("Congratulations!")
+        print("Congratulations! The customer is satfisfied!\n")
         print(f'Your wage is ${Yi.wage}')
         order_list = []
         order_earning = Yi.wage + tips ()
-        print (f'\nYour total earning for this order is ${order_earning}')
+        print (f'\nYour total earning for this order is ${order_earning}\n')
 
         order_list.append (order_earning)
 
-        print ('Would you like to continue your shift?')
-        print ('1. Yes\n2. No')
+        print ('Would you like to continue your shift?\n')
+        print ('1. Yes\n\n2. No\n')
         continue_game = input("Enter a response: ")
+        
         if continue_game.lower() == "1":
             questions()
         else:
             total_earning = sum(order_list)  
             return sprint(f'Your total earning for your shift is ${total_earning}')
     else:
-        print("Oops! That was a rude response. You lost. Game Over.")
+        customer = random.choice (customerresponses)
+        print (customer)
 questions ()
